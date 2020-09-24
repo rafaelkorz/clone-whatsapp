4378d61061dda8e3003523de9a5bdac0b8a03514
Rafael Korz <rafael.korz@gmail.com>
2020-09-24 20:58:59 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello word
+    </div>
+  );
+}
+
+export default App;
